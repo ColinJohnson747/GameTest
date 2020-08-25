d753d7a81c098b2b3a219faa75d2259ee096da60
ColinJohnson747 <60419260+ColinJohnson747@users.noreply.github.com>
2020-08-25 00:16:05 -0400

    Lets get back into the swing of things

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -15,26 +15,7 @@ function Home() {
   const formEl = useRef(null);
 
   // Load all books and store them with setBooks
-  useEffect(() => {
-    loadBooks();
-  }, []);
-
-  // Loads all books and sets them to books
-  function loadBooks() {
-    API.getBooks()
-      .then((res) => {
-        // console.log(res.data.books);
-        setBooks(res.data.books);
-      })
-      .catch((err) => console.log(err));
-  }
-
-  // Deletes a book from the database with a given id, then reloads books from the db
-  function deleteBook(id) {
-    API.deleteBook(id)
-      .then((res) => loadBooks())
-      .catch((err) => console.log(err));
-  }
+  useEffect(() => {}, []);
 
   // Handles updating component state when the user types into the input field
   function handleInputChange(event) {
@@ -46,68 +27,24 @@ function Home() {
   // Then reload books from the database
   function handleFormSubmit(event) {
     event.preventDefault();
-    if (formObject.title && formObject.author) {
-      API.saveBook({
-        title: formObject.title,
-        author: formObject.author,
-        synopsis: formObject.synopsis,
-      })
-        .then((res) => {
-          formEl.current.reset();
-          loadBooks();
-        })
-        .catch((err) => console.log(err));
-    }
   }
 
   return (
     <Container fluid>
       <Row>
-        <Col size="md-6">
-          <Card title="What Books Should I Read?">
-            <form ref={formEl}>
-              <Input
-                onChange={handleInputChange}
-                name="title"
-                placeholder="Title (required)"
-              />
-              <Input
-                onChange={handleInputChange}
-                name="author"
-                placeholder="Author (required)"
-              />
-              <TextArea
-                onChange={handleInputChange}
-                name="synopsis"
-                placeholder="Synopsis (Optional)"
-              />
-              <FormBtn
-                disabled={!(formObject.author && formObject.title)}
-                onClick={handleFormSubmit}
-              >
-                Submit Book
-              </FormBtn>
-            </form>
+        <Col size="md-4">
+          <Card>
+            <h1>Search</h1>
           </Card>
         </Col>
-        <Col size="md-6 sm-12">
-          <Card title="Books On My List">
-            {books.length ? (
-              <List>
-                {books.map((book) => (
-                  <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => deleteBook(book._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
+        <Col size="md-4">
+          <Card>
+            <h1>Create</h1>
+          </Card>
+        </Col>
+        <Col size="md-4">
+          <Card>
+            <h1>View</h1>
           </Card>
         </Col>
       </Row>
