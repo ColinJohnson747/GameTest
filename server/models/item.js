db9d3c623038b90aadf394c77081e64d3dfab11a
ColinJohnson747 <60419260+ColinJohnson747@users.noreply.github.com>
2020-10-03 21:02:24 -0400

    Added item seed and seeder

--- server/models/item.js
+++ server/models/item.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-  name: { type: String, required: true },
+  name: [{ type: String, required: true }],
   type: { type: String, required: true },
   weight: String,
-  text: String,
+  detail: String,
+  roll: String,
+  text: [{ type: String }],
+  value: String,
   date: { type: Date, default: Date.now },
 });
 
-const Item = mongoose.model("Book", bookSchema);
+const Item = mongoose.model("Item", itemSchema);
 
 module.exports = Item;
---
+++ server/seeders/seed.js
@@ -0,0 +1,19 @@
+let mongoose = require("mongoose");
+let db = require("../models");
+let itemSeed = require("../../itemSeed.json");
+
+mongoose.connect("mongodb://localhost/GameTest", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+db.Item.deleteMany({})
+  .then(() => db.business.collection.insertMany(itemSeed))
+  .then((data) => {
+    console.log(data.result.n + "records inserted");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
