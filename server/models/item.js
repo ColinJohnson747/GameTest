20fa8f9486913eaaeeb1e9941541b25a8c366e54
ColinJohnson747 <60419260+ColinJohnson747@users.noreply.github.com>
2020-10-04 01:47:52 -0400

    updated item controller

--- server/models/item.js
+++ server/models/item.js
@@ -4,12 +4,26 @@ const Schema = mongoose.Schema;
 const itemSchema = new Schema({
   name: [{ type: String, required: true }],
   type: { type: String, required: true },
-  weight: String,
-  detail: String,
-  roll: String,
+  weight: { type: String },
+  detail: { type: String },
+  roll: [{ type: String }],
   text: [{ type: String }],
-  value: String,
+  value: { type: String },
+  magic: { type: String },
+  ac: { type: String },
+  modifier: {
+    _category: { type: String },
+    __text: { type: String },
+  },
+  dmg1: { type: String },
+  dmg2: { type: String },
+  dmgType: { type: String },
   date: { type: Date, default: Date.now },
+  dmgType: { type: String },
+  property: { type: String },
+  range: { type: String },
+  strength: { type: String },
+  stealth: { type: String },
 });
 
 const Item = mongoose.model("Item", itemSchema);
