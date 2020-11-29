2b8f413fe2c8c708a13a0df5dfdb9a826bcce803
ColinJohnson747 <60419260+ColinJohnson747@users.noreply.github.com>
2020-11-29 17:35:17 -0500

    Revert "More search"
    This reverts commit f88755ea6f07fe22dd9c06b7f726155367d315ab.

--- client/src/components/SearchNav/SearchNav.js
+++ client/src/components/SearchNav/SearchNav.js
@@ -1,32 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Nav, NavItem, NavLink, TabContent, TabPane } from "reactstrap";
 import SearchBar from "../SearchBar";
 
-import API from "../../utils/API";
-
-function MyTabs(props) {
-  const [items, setItems] = useState("");
+export default function MyTabs(props) {
   const [activeTab, setActiveTab] = useState("1");
-
-  useEffect(() => {
-    let mounted = true;
-    API.getAllItems().then((res) => {
-      if (mounted) {
-        setItems(res.data);
-      }
-    });
-    return function cleanup() {
-      mounted = false;
-    };
-  }, []);
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    const search = event.target.value;
-    API.getSearchedItems(search).then((res) => {
-      setItems(res.data);
-    });
-  }
-
   return (
     <div>
       <Nav tabs>
@@ -41,12 +18,10 @@ function MyTabs(props) {
       </Nav>
       <TabContent activeTab={activeTab}>
         <TabPane tabId="1">
-          <SearchBar handleFormSubmit={handleFormSubmit} />
+          <SearchBar />
         </TabPane>
         <TabPane tabId="2">Tab 2 Content</TabPane>
       </TabContent>
     </div>
   );
 }
-
-export default MyTabs;
