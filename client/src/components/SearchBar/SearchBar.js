f88755ea6f07fe22dd9c06b7f726155367d315ab
ColinJohnson747 <60419260+ColinJohnson747@users.noreply.github.com>
2020-11-29 17:33:07 -0500

    More search

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,54 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import API from "../../utils/API";
-function SearchBar() {
-  // const [formObject, setFormObject] = useState({});
-  const [search, setSearch] = useState("");
-  const [items, setItems] = useState("");
-  let history = useHistory();
+import React, { useState } from "react";
 
-  useEffect(() => {
-    let mounted = true;
-    API.getAllItems().then((res) => {
-      if (mounted) {
-        setItems(res.data);
-      }
-    });
-    return function cleanup() {
-      mounted = false;
-    };
-  }, []);
+function SearchBar(props) {
+  const [formObject, setFormObject] = useState({});
 
-  const handleInputChange = (event) => {
-    const { value } = event.target;
-    setSearch(value);
-  };
-
-  function submit(event) {
-    event.preventDefault();
-    const search = event.target.value;
-    API.getItems(search).then((res) => {
-      setItems(res.data);
-    });
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({ ...formObject, [name]: value });
   }
 
   return (
     <div>
-      <form onSubmit={submit}>
-        <div className="form-group">
+      <form className="resultSearch">
+        <div className="form-group searchBar ">
+          {/* <label htmlFor="searchBar">Business Name</label> */}
           <input
             type="text"
             className="form-control"
-            id="searchMain"
-            aria-describedby="emailHelp"
+            id="searchBar"
+            aria-describedby="searchHelp"
+            placeholder="Please search what you would like to find."
             name="search"
-            value={search}
-            placeholder="Search Database"
             onChange={handleInputChange}
           />
+          {/* <small id="searchHelp" className="form-text text-muted">
+            Please search what you would like to find.
+          </small> */}
         </div>
+        <button
+          type="submit"
+          className="btn btn-success btn-result"
+          id="search-page-button"
+          value={formObject.search}
+          onClick={props.handleFormSubmit}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
 }
+
 export default SearchBar;
--- client/src/components/SearchNav/SearchNav.js
+++ client/src/components/SearchNav/SearchNav.js
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Nav, NavItem, NavLink, TabContent, TabPane } from "reactstrap";
 import SearchBar from "../SearchBar";
 
-export default function MyTabs(props) {
+import API from "../../utils/API";
+
+function MyTabs(props) {
+  const [items, setItems] = useState("");
   const [activeTab, setActiveTab] = useState("1");
+
+  useEffect(() => {
+    let mounted = true;
+    API.getAllItems().then((res) => {
+      if (mounted) {
+        setItems(res.data);
+      }
+    });
+    return function cleanup() {
+      mounted = false;
+    };
+  }, []);
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const search = event.target.value;
+    API.getSearchedItems(search).then((res) => {
+      setItems(res.data);
+    });
+  }
+
   return (
     <div>
       <Nav tabs>
@@ -18,10 +41,12 @@ export default function MyTabs(props) {
       </Nav>
       <TabContent activeTab={activeTab}>
         <TabPane tabId="1">
-          <SearchBar />
+          <SearchBar handleFormSubmit={handleFormSubmit} />
         </TabPane>
         <TabPane tabId="2">Tab 2 Content</TabPane>
       </TabContent>
     </div>
   );
 }
+
+export default MyTabs;
